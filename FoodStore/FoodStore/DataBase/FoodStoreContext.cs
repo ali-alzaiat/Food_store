3349040adee558514410b0561018adf9131fc6b1
ali-alzaiat <alialzaiat2000@gmail.com>
2024-03-17 22:14:50 +0200

    created Database context file

---
+++ FoodStore/FoodStore/DataBase/FoodStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using FoodStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodStore.DataBase
+{
+    public class FoodStoreContext : DbContext
+    {
+        public DbSet<Products> products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Store.db");
+        }
+    }
+}
--- FoodStore/FoodStore/Program.cs
+++ FoodStore/FoodStore/Program.cs
@@ -1,7 +1,10 @@
+using FoodStore.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<FoodStoreContext>();
 
 var app = builder.Build();
 
